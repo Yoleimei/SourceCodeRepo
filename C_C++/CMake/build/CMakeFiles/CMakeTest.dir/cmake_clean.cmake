73bb15ad64523eebb51e14a7988db0a0760e9a39
yuliming <yuliming@pset.suntec.net>
2016-06-27 17:59:50 +0800

    .

---
+++ C_C++/CMake/build/CMakeFiles/CMakeTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CMakeTest.dir/CMakeTest.cpp.o"
+  "CMakeTest.pdb"
+  "CMakeTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CMakeTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
