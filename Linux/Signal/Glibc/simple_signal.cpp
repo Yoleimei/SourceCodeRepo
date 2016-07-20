f64fdcb516997479d1e2ada688be52a1cb6eb8d9
yuliming <yuliming@pset.suntec.net>
2016-07-20 18:05:03 +0800

    .

---
+++ Linux/Signal/Glibc/simple_signal.cpp
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void sig_usr(int) 
+{
+	printf("receive user signal\n");
+}
+
+int main()
+{
+	struct sigaction sa;
+	sa.sa_flags = 0;
+	sa.sa_handler = sig_usr;
+	sigaction(SIGUSR1, &sa, NULL);
+
+	sleep(1000);
+}
