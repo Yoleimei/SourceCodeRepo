c2829ae52b23f4e9b58b1615c77e4d85134cc09f
yuliming <yuliming@pset.suntec.net>
2016-07-28 20:36:45 +0800

    .

--- Linux/Signal/Glibc/signal.cpp
+++ Linux/Signal/Glibc/signal.cpp
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <string.h>
 
 sigset_t oldset;
+bool flag = true;
 
 void sig_usr(int signum) 
 {
 	printf("receive signal: %s\n", strsignal(signum)); 
 	
-	if (SIGUSR2 == signum)
+	if (SIGUSR2 == signum) {
 		sigprocmask(SIG_SETMASK, &oldset, NULL);
+		flag = false;
+	}
 }
 
 void check_waiting_signal(const sigset_t *set, int signum)
@@ -21,7 +26,7 @@ void check_waiting_signal(const sigset_t *set, int signum)
 int main()
 {
 	struct sigaction sa;
-	sigset_t newset, pendingset;
+	sigset_t sigset, pendingset;
 	
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
@@ -29,9 +34,9 @@ int main()
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	
-	sigemptyset(&newset);
-	sigaddset(&newset, SIGUSR1);
-	sigprocmask(SIG_BLOCK, &newset, &oldset);
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigprocmask(SIG_BLOCK, &sigset, &oldset);
 	
 	sleep(5);  // only kill signal during sleep() time shall we get pending mask!
 	sigemptyset(&pendingset);
@@ -39,6 +44,24 @@ int main()
 		printf("sigpending error\n");
 	check_waiting_signal(&pendingset, SIGUSR1);
 	check_waiting_signal(&pendingset, SIGUSR2);
+	
+	printf("wait for SIGUSR2\n");
+	while (flag)
+		sleep(1);
+	
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR2);
+	printf("to do sigsuspend, SIGUSR2 is blocked\n");
+#if 0
+	// sigsuspend means doing the following code atomicly
+	sigset_t prevset;
+	sigprocmask(SIG_BLOCK, &sigset, &prevset);
+	pause();
+	sigprocmask(SIG_BLOCK, &prevset, NULL);
+#endif
+	if (sigsuspend(&sigset) < 0)  // sigsuspend will always return -1, and set errno to EINTR
+		printf("sigsuspend error: %s\n", strerror(errno));
+	printf("sigsuspend over\n");
 	
 	while (true)
 		sleep(1);
