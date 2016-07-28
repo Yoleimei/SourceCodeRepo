711a54fbdedba2b237e3614d8cb5f64991e77edd
yuliming <yuliming@pset.suntec.net>
2016-07-28 19:45:51 +0800

    .

--- Linux/Signal/Glibc/sigprocmask.cpp
+++ Linux/Signal/Glibc/sigprocmask.cpp
@@ -3,20 +3,25 @@
 #include <unistd.h>
 #include <string.h>
 
-sigset_t oldmask;
+sigset_t oldset;
 
 void sig_usr(int signum) 
 {
 	printf("receive signal: %s\n", strsignal(signum)); 
 	
 	if (SIGUSR2 == signum)
-		sigprocmask(SIG_SETMASK, &oldmask, NULL);
+		sigprocmask(SIG_SETMASK, &oldset, NULL);
+}
+
+void check_waiting_signal(const sigset_t *set, int signum)
+{
+	printf("%s is waiting: %d\n", strsignal(signum), sigismember(set, signum));
 }
 
 int main()
 {
 	struct sigaction sa;
-	sigset_t newmask;
+	sigset_t newset, pendingset;
 	
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
@@ -24,9 +29,16 @@ int main()
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	
-	sigemptyset(&newmask);
-	sigaddset(&newmask, SIGUSR1);
-	sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+	sigemptyset(&newset);
+	sigaddset(&newset, SIGUSR1);
+	sigprocmask(SIG_BLOCK, &newset, &oldset);
+	
+	sleep(5);  // only kill signal during sleep() time shall we get pending mask!
+	sigemptyset(&pendingset);
+	if (sigpending(&pendingset) < 0)
+		printf("sigpending error\n");
+	check_waiting_signal(&pendingset, SIGUSR1);
+	check_waiting_signal(&pendingset, SIGUSR2);
 	
 	while (true)
 		sleep(1);
