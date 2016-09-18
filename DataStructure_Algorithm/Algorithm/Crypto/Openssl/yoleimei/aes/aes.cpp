02b8b6c7b9e0992da93c491c18132b9adbb73f0b
yoleimei <yoleimei@163.com>
2016-09-18 11:39:42 +0800

    .

--- DataStructure_Algorithm/Algorithm/Crypto/Openssl/yoleimei/aes/aes.cpp
+++ DataStructure_Algorithm/Algorithm/Crypto/Openssl/yoleimei/aes/aes.cpp
@@ -12,9 +12,11 @@ static unsigned char iv[AES_BLOCK_SIZE];
 bool aes_encrypt(const BYTE *inbuf, UINT len, BYTE *outbuf)
 {
 	AES_KEY aes;
+	unsigned char tmp_iv[AES_BLOCK_SIZE];
+	memcpy(tmp_iv, iv, AES_BLOCK_SIZE);
 	if (AES_set_encrypt_key(key, 128, &aes) < 0)
 		return false;
-	AES_cbc_encrypt(inbuf, outbuf, len, &aes, iv, AES_ENCRYPT);
+	AES_cbc_encrypt(inbuf, outbuf, len, &aes, tmp_iv, AES_ENCRYPT);
 	
 	return true;
 }
@@ -22,9 +24,11 @@ bool aes_encrypt(const BYTE *inbuf, UINT len, BYTE *outbuf)
 bool aes_decrypt(const BYTE *inbuf, UINT len, BYTE *outbuf)
 {
 	AES_KEY aes;
+	unsigned char tmp_iv[AES_BLOCK_SIZE];
+	memcpy(tmp_iv, iv, AES_BLOCK_SIZE);
 	if (AES_set_decrypt_key(key, 128, &aes) < 0)
 		return false;
-	AES_cbc_encrypt(inbuf, outbuf, len, &aes, iv, AES_DECRYPT);
+	AES_cbc_encrypt(inbuf, outbuf, len, &aes, tmp_iv, AES_DECRYPT);
 	
 	return true;
 }
@@ -44,16 +48,17 @@ int main()
 	for (int i = 0; i < AES_BLOCK_SIZE; ++i) 
 		iv[i] = 0;
 	
-	char plaintext[AES_BLOCK_SIZE];
+	unsigned char plaintext[AES_BLOCK_SIZE] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
+	};
 	unsigned char ciphertext[AES_BLOCK_SIZE];
-	memset(plaintext, 0, AES_BLOCK_SIZE);
 	memset(ciphertext, 0, AES_BLOCK_SIZE);
-	strcpy(plaintext, "abcdefg");
-	show("==plaintext==", plaintext);
+	show("==plaintext==", plaintext, AES_BLOCK_SIZE);
 	if (!aes_encrypt((unsigned char *)plaintext, AES_BLOCK_SIZE, ciphertext))
 		printf("encrypt error\n");
-	show("==ciphertext==", ciphertext);
+	show("==ciphertext==", ciphertext, AES_BLOCK_SIZE);
 	if (!aes_decrypt(ciphertext, AES_BLOCK_SIZE, (unsigned char *)plaintext))
 		printf("decrypt error\n");
-	show("==plaintext==", plaintext);
+	show("==plaintext==", plaintext, AES_BLOCK_SIZE);
 }
