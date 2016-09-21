9d7c27c59f8eb002df577b29d7301b0d1bf67c46
yoleimei <yoleimei@163.com>
2016-09-21 20:12:20 +0800

    .

--- DataStructure_Algorithm/Algorithm/Crypto/Openssl/yoleimei/aes/aes.cpp
+++ DataStructure_Algorithm/Algorithm/Crypto/Openssl/yoleimei/aes/aes.cpp
@@ -44,9 +44,9 @@ void show(const char *title, unsigned char *data, int len)
 int main()
 {
 	for (int i = 0; i < AES_BLOCK_SIZE; ++i) 
-		key[i] = 32 + i;
+		key[i] = 96 + i;
 	for (int i = 0; i < AES_BLOCK_SIZE; ++i) 
-		iv[i] = 0;
+		iv[i] = i;
 	
 	//printf("STRICT_ALIGNMENT = %d\n", STRICT_ALIGNMENT);
 
@@ -65,7 +65,6 @@ int main()
 		printf("decrypt error\n");
 	show("==plaintext==", plaintext, AES_BLOCK_SIZE*2);
 
-
 	unsigned char plaintext1[AES_BLOCK_SIZE*2] = {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'
@@ -83,20 +82,50 @@ int main()
 		printf("decrypt error\n");
 	show("==checktext1==", checktext1, AES_BLOCK_SIZE*2);
 
-
-	unsigned char plaintext2[AES_BLOCK_SIZE+1] = {
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'
+	unsigned char plaintext2[] = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'O', 'P', 'Q'
 	};
 	unsigned char ciphertext2[AES_BLOCK_SIZE*2];
 	unsigned char checktext2[AES_BLOCK_SIZE*2];
 	memset(ciphertext2, 0, AES_BLOCK_SIZE*2);
 	memset(checktext2, 0, AES_BLOCK_SIZE*2);
-	show("==plaintext2==", plaintext2, AES_BLOCK_SIZE+1);
+	show("==plaintext2==", plaintext2, sizeof(plaintext2));
 	if (!aes_encrypt((unsigned char *)plaintext2, AES_BLOCK_SIZE*2, ciphertext2))
 		printf("encrypt error\n");
-	show("==ciphertext1==", ciphertext2, AES_BLOCK_SIZE*2);
+	show("==ciphertext2==", ciphertext2, AES_BLOCK_SIZE*2);
 	if (!aes_decrypt(ciphertext2, AES_BLOCK_SIZE*2, (unsigned char *)checktext2))
 		printf("decrypt error\n");
 	show("==checktext2==", checktext2, AES_BLOCK_SIZE*2);
+
+	unsigned char plaintext3[] = {
+		'A', '0', 'B', '1', 'C', '2', 'D', '3',
+		'E', '4', 'F', '5', 'G', '6', 'H', '7', 'I', '8', 'J', '9'
+	};
+	unsigned char ciphertext3[AES_BLOCK_SIZE*2];
+	unsigned char checktext3[AES_BLOCK_SIZE*2];
+	memset(ciphertext3, 0, AES_BLOCK_SIZE*2);
+	memset(checktext3, 0, AES_BLOCK_SIZE*2);
+	show("==plaintext3==", plaintext3, sizeof(plaintext3));
+	if (!aes_encrypt((unsigned char *)plaintext3, AES_BLOCK_SIZE*2, ciphertext3))
+		printf("encrypt error\n");
+	show("==ciphertext3==", ciphertext3, AES_BLOCK_SIZE*2);
+	if (!aes_decrypt(ciphertext3, AES_BLOCK_SIZE*2, (unsigned char *)checktext3))
+		printf("decrypt error\n");
+	show("==checktext3==", checktext3, AES_BLOCK_SIZE*2);
+
+	char str4[] = "skdbhakjfgbajkshfslah";
+	unsigned char plaintext4[strlen(str4)];
+	memcpy(plaintext4, str4, strlen(str4));
+	unsigned char ciphertext4[AES_BLOCK_SIZE*2];
+	unsigned char checktext4[AES_BLOCK_SIZE*2];
+	memset(ciphertext4, 0, AES_BLOCK_SIZE*2);
+	memset(checktext4, 0, AES_BLOCK_SIZE*2);
+	show("==plaintext4==", plaintext4, sizeof(plaintext4));
+	if (!aes_encrypt((unsigned char *)plaintext4, AES_BLOCK_SIZE*2, ciphertext4))
+		printf("encrypt error\n");
+	show("==ciphertext4==", ciphertext4, AES_BLOCK_SIZE*2);
+	if (!aes_decrypt(ciphertext4, AES_BLOCK_SIZE*2, (unsigned char *)checktext4))
+		printf("decrypt error\n");
+	show("==checktext4==", checktext4, AES_BLOCK_SIZE*2);
 }
