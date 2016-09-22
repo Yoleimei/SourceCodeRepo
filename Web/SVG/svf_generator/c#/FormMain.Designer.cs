22f5755796d560048428c6b1d875e63ea5312711
yuliming <yuliming@pset.suntec.net>
2016-09-22 17:49:20 +0800

    .

---
+++ Web/SVG/svf_generator/c#/FormMain.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Test
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelRect = new System.Windows.Forms.Label();
+            this.labelCircle = new System.Windows.Forms.Label();
+            this.labelRectX = new System.Windows.Forms.Label();
+            this.textBoxRectX = new System.Windows.Forms.TextBox();
+            this.labelCircleCx = new System.Windows.Forms.Label();
+            this.textBoxCircleCx = new System.Windows.Forms.TextBox();
+            this.textBoxRectY = new System.Windows.Forms.TextBox();
+            this.labelRectY = new System.Windows.Forms.Label();
+            this.textBoxCircleCy = new System.Windows.Forms.TextBox();
+            this.labelCircleCy = new System.Windows.Forms.Label();
+            this.textBoxRectWidth = new System.Windows.Forms.TextBox();
+            this.labelRectWidth = new System.Windows.Forms.Label();
+            this.textBoxCircleR = new System.Windows.Forms.TextBox();
+            this.labelCircleR = new System.Windows.Forms.Label();
+            this.textBoxRectHeight = new System.Windows.Forms.TextBox();
+            this.labelRectHeight = new System.Windows.Forms.Label();
+            this.buttonRect = new System.Windows.Forms.Button();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelRect
+            // 
+            this.labelRect.AutoSize = true;
+            this.labelRect.Location = new System.Drawing.Point(20, 30);
+            this.labelRect.Name = "labelRect";
+            this.labelRect.Size = new System.Drawing.Size(29, 12);
+            this.labelRect.TabIndex = 0;
+            this.labelRect.Text = "矩形";
+            // 
+            // labelCircle
+            // 
+            this.labelCircle.AutoSize = true;
+            this.labelCircle.Location = new System.Drawing.Point(20, 60);
+            this.labelCircle.Name = "labelCircle";
+            this.labelCircle.Size = new System.Drawing.Size(17, 12);
+            this.labelCircle.TabIndex = 1;
+            this.labelCircle.Text = "圆";
+            // 
+            // labelRectX
+            // 
+            this.labelRectX.AutoSize = true;
+            this.labelRectX.Location = new System.Drawing.Point(60, 30);
+            this.labelRectX.Name = "labelRectX";
+            this.labelRectX.Size = new System.Drawing.Size(11, 12);
+            this.labelRectX.TabIndex = 2;
+            this.labelRectX.Text = "x";
+            // 
+            // textBoxRectX
+            // 
+            this.textBoxRectX.Location = new System.Drawing.Point(100, 30);
+            this.textBoxRectX.Name = "textBoxRectX";
+            this.textBoxRectX.Size = new System.Drawing.Size(100, 21);
+            this.textBoxRectX.TabIndex = 3;
+            // 
+            // labelCircleCx
+            // 
+            this.labelCircleCx.AutoSize = true;
+            this.labelCircleCx.Location = new System.Drawing.Point(60, 60);
+            this.labelCircleCx.Name = "labelCircleCx";
+            this.labelCircleCx.Size = new System.Drawing.Size(17, 12);
+            this.labelCircleCx.TabIndex = 4;
+            this.labelCircleCx.Text = "cx";
+            // 
+            // textBoxCircleCx
+            // 
+            this.textBoxCircleCx.Location = new System.Drawing.Point(100, 60);
+            this.textBoxCircleCx.Name = "textBoxCircleCx";
+            this.textBoxCircleCx.Size = new System.Drawing.Size(100, 21);
+            this.textBoxCircleCx.TabIndex = 5;
+            // 
+            // textBoxRectY
+            // 
+            this.textBoxRectY.Location = new System.Drawing.Point(260, 30);
+            this.textBoxRectY.Name = "textBoxRectY";
+            this.textBoxRectY.Size = new System.Drawing.Size(100, 21);
+            this.textBoxRectY.TabIndex = 7;
+            // 
+            // labelRectY
+            // 
+            this.labelRectY.AutoSize = true;
+            this.labelRectY.Location = new System.Drawing.Point(220, 30);
+            this.labelRectY.Name = "labelRectY";
+            this.labelRectY.Size = new System.Drawing.Size(11, 12);
+            this.labelRectY.TabIndex = 6;
+            this.labelRectY.Text = "y";
+            // 
+            // textBoxCircleCy
+            // 
+            this.textBoxCircleCy.Location = new System.Drawing.Point(260, 60);
+            this.textBoxCircleCy.Name = "textBoxCircleCy";
+            this.textBoxCircleCy.Size = new System.Drawing.Size(100, 21);
+            this.textBoxCircleCy.TabIndex = 9;
+            // 
+            // labelCircleCy
+            // 
+            this.labelCircleCy.AutoSize = true;
+            this.labelCircleCy.Location = new System.Drawing.Point(220, 60);
+            this.labelCircleCy.Name = "labelCircleCy";
+            this.labelCircleCy.Size = new System.Drawing.Size(17, 12);
+            this.labelCircleCy.TabIndex = 8;
+            this.labelCircleCy.Text = "cy";
+            // 
+            // textBoxRectWidth
+            // 
+            this.textBoxRectWidth.Location = new System.Drawing.Point(420, 30);
+            this.textBoxRectWidth.Name = "textBoxRectWidth";
+            this.textBoxRectWidth.Size = new System.Drawing.Size(100, 21);
+            this.textBoxRectWidth.TabIndex = 11;
+            // 
+            // labelRectWidth
+            // 
+            this.labelRectWidth.AutoSize = true;
+            this.labelRectWidth.Location = new System.Drawing.Point(380, 30);
+            this.labelRectWidth.Name = "labelRectWidth";
+            this.labelRectWidth.Size = new System.Drawing.Size(35, 12);
+            this.labelRectWidth.TabIndex = 10;
+            this.labelRectWidth.Text = "witdh";
+            // 
+            // textBoxCircleR
+            // 
+            this.textBoxCircleR.Location = new System.Drawing.Point(420, 60);
+            this.textBoxCircleR.Name = "textBoxCircleR";
+            this.textBoxCircleR.Size = new System.Drawing.Size(100, 21);
+            this.textBoxCircleR.TabIndex = 13;
+            // 
+            // labelCircleR
+            // 
+            this.labelCircleR.AutoSize = true;
+            this.labelCircleR.Location = new System.Drawing.Point(380, 60);
+            this.labelCircleR.Name = "labelCircleR";
+            this.labelCircleR.Size = new System.Drawing.Size(11, 12);
+            this.labelCircleR.TabIndex = 12;
+            this.labelCircleR.Text = "r";
+            // 
+            // textBoxRectHeight
+            // 
+            this.textBoxRectHeight.Location = new System.Drawing.Point(580, 30);
+            this.textBoxRectHeight.Name = "textBoxRectHeight";
+            this.textBoxRectHeight.Size = new System.Drawing.Size(100, 21);
+            this.textBoxRectHeight.TabIndex = 15;
+            // 
+            // labelRectHeight
+            // 
+            this.labelRectHeight.AutoSize = true;
+            this.labelRectHeight.Location = new System.Drawing.Point(540, 30);
+            this.labelRectHeight.Name = "labelRectHeight";
+            this.labelRectHeight.Size = new System.Drawing.Size(41, 12);
+            this.labelRectHeight.TabIndex = 14;
+            this.labelRectHeight.Text = "height";
+            // 
+            // buttonRect
+            // 
+            this.buttonRect.Location = new System.Drawing.Point(700, 30);
+            this.buttonRect.Name = "buttonRect";
+            this.buttonRect.Size = new System.Drawing.Size(75, 23);
+            this.buttonRect.TabIndex = 16;
+            this.buttonRect.Text = "生成";
+            this.buttonRect.UseVisualStyleBackColor = true;
+            this.buttonRect.Click += new System.EventHandler(this.buttonRect_Click);
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(700, 60);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(75, 23);
+            this.buttonCircle.TabIndex = 16;
+            this.buttonCircle.Text = "生成";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.buttonCircle_Click);
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(12, 124);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(760, 425);
+            this.textBoxCode.TabIndex = 17;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(22, 95);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 18;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.buttonCircle);
+            this.Controls.Add(this.buttonRect);
+            this.Controls.Add(this.textBoxRectHeight);
+            this.Controls.Add(this.labelRectHeight);
+            this.Controls.Add(this.textBoxCircleR);
+            this.Controls.Add(this.labelCircleR);
+            this.Controls.Add(this.textBoxRectWidth);
+            this.Controls.Add(this.labelRectWidth);
+            this.Controls.Add(this.textBoxCircleCy);
+            this.Controls.Add(this.labelCircleCy);
+            this.Controls.Add(this.textBoxRectY);
+            this.Controls.Add(this.labelRectY);
+            this.Controls.Add(this.textBoxCircleCx);
+            this.Controls.Add(this.labelCircleCx);
+            this.Controls.Add(this.textBoxRectX);
+            this.Controls.Add(this.labelRectX);
+            this.Controls.Add(this.labelCircle);
+            this.Controls.Add(this.labelRect);
+            this.Name = "FormMain";
+            this.Text = "SVG生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRect;
+        private System.Windows.Forms.Label labelCircle;
+        private System.Windows.Forms.Label labelRectX;
+        private System.Windows.Forms.TextBox textBoxRectX;
+        private System.Windows.Forms.Label labelCircleCx;
+        private System.Windows.Forms.TextBox textBoxCircleCx;
+        private System.Windows.Forms.TextBox textBoxRectY;
+        private System.Windows.Forms.Label labelRectY;
+        private System.Windows.Forms.TextBox textBoxCircleCy;
+        private System.Windows.Forms.Label labelCircleCy;
+        private System.Windows.Forms.TextBox textBoxRectWidth;
+        private System.Windows.Forms.Label labelRectWidth;
+        private System.Windows.Forms.TextBox textBoxCircleR;
+        private System.Windows.Forms.Label labelCircleR;
+        private System.Windows.Forms.TextBox textBoxRectHeight;
+        private System.Windows.Forms.Label labelRectHeight;
+        private System.Windows.Forms.Button buttonRect;
+        private System.Windows.Forms.Button buttonCircle;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ Web/SVG/svf_generator/c#/FormMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRect_Click(object sender, EventArgs e)
+        {
+            string str = string.Format("<rect x=\"{0}\" y=\"{1}\" width=\"{2}\" height=\"{3}\"/>",
+                textBoxRectX.Text, textBoxRectY.Text, textBoxRectWidth.Text, textBoxRectHeight.Text);
+            textBoxCode.Text += "\r\n" + str + "\r\n";
+        }
+
+        private void buttonCircle_Click(object sender, EventArgs e)
+        {
+            string str = string.Format("<circle cx =\"{0}\" cy=\"{1}\" r=\"{2}\"/>",
+                textBoxCircleCx.Text, textBoxCircleCy.Text, textBoxCircleR.Text);
+            textBoxCode.Text += "\r\n" + str + "\r\n";
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = File.Create("text.svg"))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("<?xml version=\"1.0\" standalone=\"no\"?>");
+                    sw.WriteLine("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">");
+                    sw.WriteLine("<svg width=\"100%\" height=\"100%\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">");
+                    sw.Write(textBoxCode.Text);
+                    sw.WriteLine("</svg>");
+                }
+            }
+        }
+    }
+}
