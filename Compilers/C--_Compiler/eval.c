014d474e33fe7bcc31b2c6dbafcaa1136e1801f4
yoleimei <yoleimei@163.com>
2016-10-01 00:09:51 +0800

    .

---
+++ Compilers/C--_Compiler/eval.c
@@ -0,0 +1,44 @@
+#ifndef EVAL_H
+	#include "eval.h"
+#endif
+
+int eval()
+{
+	int op, *tmp;
+	while (1) {
+		op = *pc++;
+		if (INSTRUCTION_IMM == op)       { ax = *pc++; }
+		else if (INSTRUCTION_LC == op)   { ax = *(char *)ax; }
+		else if (INSTRUCTION_LI == op)   { ax = *(int *)ax; }
+		else if (INSTRUCTION_SC == op)   { *(char *)*sp++ = ax; }
+		else if (INSTRUCTION_SI == op)   { *(int *)*sp++ = ax; }
+		else if (INSTRUCTION_PUSH == op) { *--sp = ax; }
+		else if (INSTRUCTION_JMP == op)  { pc = (int *)*pc; }
+		else if (INSTRUCTION_JZ == op)   { ax ? (pc + 1) : (int *)*pc; }
+		else if (INSTRUCTION_JNZ == op)  { ax ? (int *)*pc : (pc + 1); }
+		else if (INSTRUCTION_CALL == op) { *--sp = (int)(pc + 1); pc = (int *)*pc; }
+		else if (INSTRUCTION_ENT == op)  { *--sp = (int)bp; bp = sp; sp = sp - *pc++; }
+		else if (INSTRUCTION_ADJ == op)  { sp = sp + *pc++; }
+		else if (INSTRUCTION_LEV == op)  { sp = bp; bp = (int *)*sp++; pc = (int *)sp++; }
+		else if (INSTRUCTION_LEV == op)  { ax = (int)(bp + *pc++); }
+	}
+}
+
+/*
+foo(arg1, arg2);
+|       ...       |
++-----------------+
+| arg2            |
++-----------------+
+| arg1            |
++-----------------+
+| return address  |
++-----------------+
+| old bp          |
++-----------------+
+| local var1      |
++-----------------+
+| local var2      |
++-----------------+
+|       ...       |
+*/
--- Compilers/C--_Compiler/eval.h
+++ Compilers/C--_Compiler/eval.h
@@ -2,20 +2,43 @@
 #define EVAL_H
 
 enum {
-	INSTRUCTION_IMM,   // IMM <num>   : ax <- num             premiss: ax = address
-	INSTRUCTION_LC ,   // LC          : ax <- *address(char)  premiss: ax = address of char
-	INSTRUCTION_LI ,   // LI          : ax <- *address(int)   premiss: ax = address of int
-	INSTRUCTION_SC ,   // SC          : *address <- ax(char)  premiss: stack top = address of int
-	INSTRUCTION_SI ,   // SI          : *address <- ax(int)   premiss: stack top = address of int
+	INSTRUCTION_IMM,   // IMM <num>    : ax <- num             premiss: ax = address
+	INSTRUCTION_LC ,   // LC           : ax <- *address(char)  premiss: ax = address of char
+	INSTRUCTION_LI ,   // LI           : ax <- *address(int)   premiss: ax = address of int
+	INSTRUCTION_SC ,   // SC           : *address <- ax(char)  premiss: stack top = address of int
+	INSTRUCTION_SI ,   // SI           : *address <- ax(int)   premiss: stack top = address of int
 
-	INSTRUCTION_PUSH,  // PUSH        : stack top <- ax
+	INSTRUCTION_PUSH,  // PUSH         : stack top <- ax
 
-	INSTRUCTION_JMP,   // JMP <addr>  : pc <- address
-	INSTRUCTION_JZ,    // JZ  <addr>  : pc <- address (if ax == 0)
-	INSTRUCTION_JNZ,   // JNZ <addr>  : pc <- address (if ax != 0)
+	INSTRUCTION_JMP,   // JMP <addr>   : pc <- address
+	INSTRUCTION_JZ,    // JZ  <addr>   : pc <- address (if ax == 0)
+	INSTRUCTION_JNZ,   // JNZ <addr>   : pc <- address (if ax != 0)
 
-	INSTRUCTION_CALL,  // CALL <addr> : 
-	INSTRUCTION_RET,   // RET         : 
-}
+	INSTRUCTION_CALL,  // CALL <addr>  : push return address, jmp to callee
+//	INSTRUCTION_RET,   // RET          : 
+	INSTRUCTION_ENT,   // ENT <size>   : push current stack base pointer, reserve space for arguments
+	INSTRUCTION_ADJ,   // ADJ <size>   : remove arguments from stack
+	INSTRUCTION_LEV,   // LEV          : restore sp, bp, pc
+	INSTRUCTION_LEA,   // LEA <offset> : get argument
+
+	INSTRUCTION_OR,
+	INSTRUCTION_XOR,
+	INSTRUCTION_AND,
+	INSTRUCTION_EQ,
+	INSTRUCTION_NE,
+	INSTRUCTION_LT,
+	INSTRUCTION_GT,
+	INSTRUCTION_LE,
+	INSTRUCTION_GE,
+	INSTRUCTION_SHL,
+	INSTRUCTION_SHR,
+	INSTRUCTION_ADD,
+	INSTRUCTION_SUB,
+	INSTRUCTION_MUL,
+	INSTRUCTION_DIV,
+	INSTRUCTION_MOD
+};
+
+extern int eval();
 
 #endif
