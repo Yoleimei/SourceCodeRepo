7409cc858fcabc36e2e456890aa0535c539a7d6e
yuliming <yuliming@pset.suntec.net>
2016-10-10 15:36:33 +0800

    .

--- ProgrammingLanguage/Python/FileIO/fileio.py
+++ ProgrammingLanguage/Python/FileIO/fileio.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 
 fp = open('./test.txt', 'w')
-fp.write('Hello world!')
+fp.write('Hello world!\n')
+fp.write('Hello yoleimei!\n')
 fp.close()
 
 fp = open('./test.txt', 'r')
 ret = fp.readline()
 print(ret)
+
+fp = open('./test.txt', 'r')
+ret = fp.readlines()
+for i in ret:
+	print(i)
